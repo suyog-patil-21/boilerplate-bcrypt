25c4d53a8042fecabed96cb03b94e0a67abd10bc
Suyog Patil <suyog0057@gmail.com>
2024-03-21 08:30:59 +0000

    Hash and Compare Passwords Asynchronously complete

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res); //true
+    });
+  });
 
 
 //END_ASYNC
@@ -47,7 +52,9 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 
-
+app.get('/',(req,res) =>{
+    res.send('Understand BCrypt Hashes');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
